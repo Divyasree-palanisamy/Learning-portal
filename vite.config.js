603df0fed720807b696ce570326484a39455589b
Divyasree-palanisamy <142414497+Divyasree-palanisamy@users.noreply.github.com>
2025-06-28 17:05:59 +0530

    Added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          animation: ['framer-motion', 'react-confetti'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['react-confetti'],
+  },
 })
